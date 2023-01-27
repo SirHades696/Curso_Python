e0112615a48bc4442c783a6633043ce104262b08
Emmanuel Nicanor <djnonasrm@gmail.com>
2023-01-27 09:45:58 -0600

    Script completo mapa

--- Ejercicios/seccion4/mapas_covid.py
+++ Ejercicios/seccion4/mapas_covid.py
@@ -26,7 +26,7 @@ path_op = r"/home/hades/Documentos/Curso Python/Curso_Python/Ejercicios/seccion4
 
 data_field_calculator = {
         "INPUT": layer,
-        "FIELD_NAME": "porc_covid",
+        "FIELD_NAME": "por_cov", #Los nombres de los campos no deben exceder los 8 caracteres
         "FIELD_TYPE":0,
         "FIELD_LENGTH":10,
         "FIELD_PRECISION":4, #FIELD_LENGTH_PRECISION 000000.0000
@@ -41,17 +41,23 @@ new_layer = QgsVectorLayer(resultado["OUTPUT"],layer.name() + "_v2","ogr")
 QgsProject.instance().addMapLayer(new_layer)
 
 # ---- Inicia la categorizacion "Mapa Tematico"
-symbol = QgsFillSymbol()
+symbol = QgsFillSymbol() #Hace referencia a los vectoriales del tipo poligono
 clasificacion = [QgsGraduatedSymbolRenderer.Quantile]
 style = QgsStyle.defaultStyle()
 color_ramp = style.colorRampNames() # Contiene una lista con todas las rampas de colores
 indice_ramp = color_ramp.index("YlOrRd") # Indice de la rampa  de amarillos a rojos
 ramp = style.colorRamp(color_ramp[indice_ramp]) # Aplicar el estilo de rampa seleccionado
 #el campo(Expresion) que vamos a clasificar
-field = "porc_covid"
+field = "por_cov"
 renderer = QgsGraduatedSymbolRenderer.createRenderer(new_layer,field,5,clasificacion[0],symbol,ramp)
 # Recibe primero el obj layer, recibe el campo, recibe el numero de clases, recibe el tipo de clasificacion, recibe el simbolo y la rampa de color
 new_layer.setRenderer(renderer)
+path_qml = new_layer.source().split(".shp")[0] #obtenemos la ruta de almacenamiento del nuevo archivo, eliminamos la extension ".shp" para agregar la extension ".qml"
+# path_qml = ".../Ejercicios/seccion4/Tamaulipas_v2"
+new_layer.saveNamedStyle(path_qml+".qml") #Guarda el tematico aplicado con una extension .qml
+
+
+
 
 
 
